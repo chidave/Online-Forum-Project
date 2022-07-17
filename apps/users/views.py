1bc56956f78e51dcb5da3cd8718945fc6c5113bd
chigozie.onyeije <chigozie.onyeije@fdmgroup.com>
2022-07-17 18:30:37 -0400

    Added some templates, including a base template

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def signup(request):
+    return render(request, "users/signup.html")
+
+def login(request):
+    return render(request, "users/login.html")
