2e0c6bcd3e47d535a430c74489e26a217aedc6ba
chigozie.onyeije <chigozie.onyeije@fdmgroup.com>
2022-07-17 09:47:52 -0400

    initial commit

---
+++ apps/users/models.py
@@ -0,0 +1,54 @@
+import django
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+# Create your models here.
+
+class AppUserManager(BaseUserManager):
+    
+    def create_user(self, email, username, first_name, password, **other_fields):
+        if not email:
+            raise ValueError(gettext_lazy('The Email must be set'))
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, first_name=first_name, **other_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email, username, first_name, password, **other_fields):
+
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError(gettext_lazy('Superuser must have is_staff=True.'))
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(gettext_lazy('Superuser must have is_superuser=True.'))
+
+        return self.create_user(email, username, first_name, password, **other_fields)
+
+
+
+class AppUser(AbstractBaseUser, PermissionsMixin):
+    
+    email = models.EmailField(gettext_lazy('email address'), max_length=254, unique=True)
+    username = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=150)
+    created_date = models.DateTimeField(default=timezone.now)
+    about = models.TextField(gettext_lazy('about'), max_length=500, blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+    objects = AppUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name']
+
+    def __str__(self):
+        return self.username
+    
