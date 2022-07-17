1edb0413bb84212a75c497cabc929df19077a57d
chidave <goziedavis@gmail.com>
2022-07-17 19:34:47 -0400

    Created sign up page to allow user creation w/o Django Admin

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpRequest
+
+from .forms import RegisterForm
 
 # Create your views here.
 
-def signup(request):
-    return render(request, "users/signup.html")
+def signup(request: HttpRequest):
+    form = RegisterForm()
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
+    return render(request, "users/signup.html", context)
 
 def login(request):
     return render(request, "users/login.html")
