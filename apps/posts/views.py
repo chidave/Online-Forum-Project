ac8191b535f09af0c7a7cb849ba8c663e2d7ba7d
chidave <goziedavis@gmail.com>
2022-07-31 13:24:41 -0400

    Now showing saved posts on website homepage

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -2,11 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
+from .models import Post
+
 # Create your views here.
 
 # @login_required(login_url='login')
 def index(request):
-    return render(request, "posts/index.html")
+
+    posts = Post.objects.all()
+    # for post in posts:
+    #     print(f'Title: {post.title}, Author: {post.author}, Date: {post.created_on}')
+
+    context = {'posts': posts}
+
+    return render(request, "posts/index.html", context)
 
 def newPost(request):
     return HttpResponse("Welcome to the new posts page!")
